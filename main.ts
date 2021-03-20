3c3292361bb640d185c2940690e0b3652fcb6897
nono38 <74985866+nono38@users.noreply.github.com>
2021-03-20 18:33:02 +0100

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,14 +1,14 @@
 radio.onReceivedNumber(function (receivedNumber) {
     if (receivedNumber == 1) {
         SuperBit.Servo(SuperBit.enServo.S8, 89)
-        basic.pause(1)
-        basic.pause(1)
+        basic.pause(100)
         SuperBit.Servo(SuperBit.enServo.S8, 15)
         SuperBit.Servo(SuperBit.enServo.S8, 5)
-    } else if (receivedNumber == 2) {
+        basic.pause(100)
+    } else if (receivedNumber == 3) {
         SuperBit.MotorRun(SuperBit.enMotors.M3, -255)
         SuperBit.MotorRun(SuperBit.enMotors.M2, 255)
-    } else if (receivedNumber == 3) {
+    } else if (receivedNumber == 2) {
         SuperBit.MotorRun(SuperBit.enMotors.M3, 255)
         SuperBit.MotorRun(SuperBit.enMotors.M2, -255)
     } else if (receivedNumber == 4) {
