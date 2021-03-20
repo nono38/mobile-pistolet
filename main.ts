1be86789a5e6db7c1ce6af684957f495f2c4c7d6
nono38 <74985866+nono38@users.noreply.github.com>
2021-03-20 17:59:19 +0100

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,23 @@
-input.onButtonPressed(Button.AB, function () {
-    SuperBit.Servo(SuperBit.enServo.S8, 15)
-    basic.pause(1000)
-    SuperBit.Servo(SuperBit.enServo.S8, 0)
-    SuperBit.MotorStopAll()
+radio.onReceivedNumber(function (receivedNumber) {
+    if (receivedNumber == 1) {
+        SuperBit.Servo(SuperBit.enServo.S8, 89)
+        basic.pause(1)
+        basic.pause(1)
+        SuperBit.Servo(SuperBit.enServo.S8, 15)
+        SuperBit.Servo(SuperBit.enServo.S8, 5)
+    } else if (receivedNumber == 2) {
+        SuperBit.MotorRun(SuperBit.enMotors.M3, -255)
+        SuperBit.MotorRun(SuperBit.enMotors.M2, 255)
+    } else if (receivedNumber == 3) {
+        SuperBit.MotorRun(SuperBit.enMotors.M3, 255)
+        SuperBit.MotorRun(SuperBit.enMotors.M2, -255)
+    } else if (receivedNumber == 4) {
+        SuperBit.MotorRun(SuperBit.enMotors.M3, 255)
+        SuperBit.MotorRun(SuperBit.enMotors.M2, 255)
+    } else if (receivedNumber == 5) {
+        SuperBit.MotorRun(SuperBit.enMotors.M3, 0)
+        SuperBit.MotorRun(SuperBit.enMotors.M2, 0)
+    }
 })
-basic.forever(function () {
-	
-})
+radio.setGroup(152)
+SuperBit.Servo(SuperBit.enServo.S8, 25)
