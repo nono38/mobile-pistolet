08da3789a3211fd746163c2effb6356b2be6a2e5
nono38 <74985866+nono38@users.noreply.github.com>
2021-03-27 20:18:58 +0100

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,9 @@
 radio.onReceivedNumber(function (receivedNumber) {
     if (receivedNumber == 1) {
-        SuperBit.Servo(SuperBit.enServo.S8, 89)
+        SuperBit.Servo(SuperBit.enServo.S4, 89)
         basic.pause(100)
-        SuperBit.Servo(SuperBit.enServo.S8, 15)
-        SuperBit.Servo(SuperBit.enServo.S8, 5)
+        SuperBit.Servo(SuperBit.enServo.S4, 15)
+        SuperBit.Servo(SuperBit.enServo.S4, 5)
         basic.pause(100)
     } else if (receivedNumber == 3) {
         SuperBit.MotorRun(SuperBit.enMotors.M3, -255)
@@ -17,7 +17,10 @@ radio.onReceivedNumber(function (receivedNumber) {
     } else if (receivedNumber == 5) {
         SuperBit.MotorRun(SuperBit.enMotors.M3, 0)
         SuperBit.MotorRun(SuperBit.enMotors.M2, 0)
+    } else if (receivedNumber == 6) {
+        SuperBit.MotorRun(SuperBit.enMotors.M3, -255)
+        SuperBit.MotorRun(SuperBit.enMotors.M2, -255)
     }
 })
+SuperBit.Servo(SuperBit.enServo.S4, 25)
 radio.setGroup(152)
-SuperBit.Servo(SuperBit.enServo.S8, 25)
